639d8212ab41abbf93a2f50d8ed29f42f1a5f40a
Tal Ben Yakar <talby10@gmail.com>
2015-04-30 17:28:58 +0300

    update

--- testing.py
+++ testing.py
@@ -4,4 +4,9 @@ from model import testing
 
 dataf = cPickle.load(open("dataf.p", "rb"))
 gs_logreg = cPickle.load(open("gs_logreg.p", "rb"))
-print(testing('tal', gs_logreg))
+
+#type the string
+string = 'the bloomberg  weather: today is rainy'
+string2 = 'i played basketball'
+print(testing(string, gs_logreg))
+print(testing(string2, gs_logreg))
